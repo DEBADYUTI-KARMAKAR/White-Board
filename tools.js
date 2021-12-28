b9c7bb5be135c8734686eb40dd4253b41585d063
unknown <debadyuti.karmakar@gmail.com>
2021-12-28 23:10:35 +0530

    tools container off on

--- tools.js
+++ tools.js
@@ -1,4 +1,6 @@
 let optionsCont = document.querySelector(".options-cont");
+let toolsCont = document.querySelector(".tools-cont");
+
 let optionsFlag = true;
 
 // true -> tools show , fals -> hide tools
@@ -19,9 +21,11 @@ function openTools() {
     let iconElem = optionsCont.children[0];
     iconElem.classList.remove("fa-times");
     iconElem.classList.add("fa-bars");
+    toolsCont.style.display = "none";
 }
 function closeTools() {
     let iconElem = optionsCont.children[0];
     iconElem.classList.remove("fa-bars");
     iconElem.classList.add("fa-times");
+    toolsCont.style.display = "flex";
 }
