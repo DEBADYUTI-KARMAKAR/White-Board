8a438f81c7b27ac4fced506bc81ba0620793c209
unknown <debadyuti.karmakar@gmail.com>
2021-12-29 13:36:49 +0530

    Update on off tool

--- tools.js
+++ tools.js
@@ -1,6 +1,7 @@
 let optionsCont = document.querySelector(".options-cont");
 let toolsCont = document.querySelector(".tools-cont");
-
+let pencilToolCont = document.querySelector(".pencil-tool-cont")
+let eraserToolCont = document.querySelector(".eraser-tool-cont");
 let optionsFlag = true;
 
 // true -> tools show , fals -> hide tools
@@ -22,10 +23,13 @@ function openTools() {
     iconElem.classList.remove("fa-times");
     iconElem.classList.add("fa-bars");
     toolsCont.style.display = "none";
+    pencilToolCont.style.display = "none";
+    eraserToolCont.style.display = "none";
 }
 function closeTools() {
     let iconElem = optionsCont.children[0];
     iconElem.classList.remove("fa-bars");
     iconElem.classList.add("fa-times");
     toolsCont.style.display = "flex";
+    
 }
